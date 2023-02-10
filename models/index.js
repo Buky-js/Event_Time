484d92b70abc7e6d28b7fcd28f43d04f8595fd07
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-02-09 20:23:36 -0400

    Changed index model

--- models/index.js
+++ models/index.js
@@ -2,31 +2,46 @@ const User = require('./User');
 const Category = require('./Category');
 const Event = require('./Event');
 const Order = require('./Order');
-const saved_event = require('./saved_event');
+const Saved_event = require('./saved_event');
 
-
-Category.hasMany(Event, {
-    foreignKey: 'category_id',
+Order.belongsTo(Event,{
+    foreignKey: 'event_id'
 });
 
-// Event.belongsTo(Category, {
-//     foreignKey: 'category_id',
-// });
-
-Order.hasMany(Event, {
-    foreignKey: 'event_id',
+Event.hasMany(Order,{
+    foreignKey: 'event_id'
 });
 
-Order.hasOne(User, {
-    foreignKey: 'user_id',
+Order.belongsTo(User,{
+    foreignKey: 'user_id'
 });
 
-saved_event.hasMany(Event, {
-    foreignKey: 'event_id',
+User.hasMany(Order,{
+    foreignKey: 'user_id'
 });
 
+Saved_event.belongsTo(User,{
+    foreignKey: 'user_id'
+});
 
+User.hasMany(Saved_event,{
+    foreignKey: 'user_id'
+});
 
+Saved_event.belongsTo(Event,{
+    foreignKey: 'event_id'
+});
 
+Event.hasMany(Saved_event,{
+    foreignKey: 'event_id'
+});
 
-module.exports = { Category, Event, Order, saved_event, User };
+Event.belongsTo(Category,{
+    foreignKey: 'category_id'
+});
+
+Category.hasMany(Event,{
+    foreignKey: 'category_id'
+})
+
+module.exports = { Category, Event, Order, Saved_event, User };
