b8d70fb3233f7aab0078bc03073ec28979ff21ba
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-02-09 20:02:20 -0400

    solved conflicts

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const eventRoutes = require('./eventRoutes');
-
-router.use('/users', userRoutes);
-router.use('/event', eventRoutes);
-
-
-module.exports = router;
